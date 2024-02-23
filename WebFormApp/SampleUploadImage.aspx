5daed9c779b57e5f984d90e42727a5e563a9e13b
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-02-23 15:41:48 +0700

    menambahkan upload image

---
+++ WebFormApp/SampleUploadImage.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="SampleUploadImage.aspx.vb" Inherits="WebFormApp.SampleUploadImage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Upload Image</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label for="fpGambar">MAsukan Gambar :</label>
+            <asp:FileUpload ID="fpGambar" runat="server" />
+            <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click"
+                Text="Upload" /><br />
+            <asp:Label ID="lblStatus" runat="server" />
+            <hr />
+            <asp:DataList ID="dlGambar" runat="server" RepeatColumns="3">
+                <ItemTemplate>
+                    <asp:Image ID="Image1" runat="server"
+                        ImageUrl='<%# Eval("Name", "~/Images/{0}") %>' Style="width: 200px;padding:5px;margin:10px;" />
+                </ItemTemplate>
+            </asp:DataList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormApp/SampleUploadImage.aspx.vb
@@ -0,0 +1,40 @@
+﻿Imports System.IO
+
+Public Class SampleUploadImage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Page_PreRender() Handles Me.PreRender
+        Dim dir As New DirectoryInfo(Server.MapPath("~/Images/"))
+        dlGambar.DataSource = dir.GetFiles()
+        dlGambar.DataBind()
+    End Sub
+
+    Function CekFileExtension(ByVal fileName As String) As Boolean
+        Dim ext As String = System.IO.Path.GetExtension(fileName)
+        Select Case ext.ToLower()
+            Case ".jpg", ".jpeg", ".png", ".gif"
+                Return True
+            Case Else
+                Return False
+        End Select
+    End Function
+
+    Protected Sub btnUpload_Click(sender As Object, e As EventArgs)
+        If fpGambar.HasFiles Then
+            If CekFileExtension(fpGambar.FileName) Then
+                Dim path As String = Server.MapPath("~/Images/")
+                Dim fileName As String = System.IO.Path.GetFileName(fpGambar.FileName)
+                fpGambar.SaveAs(path & Guid.NewGuid.ToString & "-" & fileName)
+                lblStatus.Text = "File berhasil diupload"
+            Else
+                lblStatus.Text = "File harus berupa gambar"
+            End If
+        Else
+            lblStatus.Text = "Pilih file terlebih dahulu"
+        End If
+    End Sub
+End Class
